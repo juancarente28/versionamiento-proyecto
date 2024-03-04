e798bf92145616aa8641d2c1082bb9656356c291
juancarente28 <115377405+juancarente28@users.noreply.github.com>
2024-03-04 00:16:05 -0500

    commiit 6
    creación de la carpeta salir

--- formulario/menu.php
+++ formulario/menu.php
@@ -1,1 +1,31 @@
 <?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Bien venido</title>
+    <link rel="stylesheet" href="style.css">
+    </style>
+
+   
+      
+</head>
+
+<body>
+<div class="page-header">
+    <h3>Hola <span><?php echo htmlspecialchars($_SESSION["username"]); ?></span> Bienvenido.</h3>
+</div>
+</body>
+</html>
